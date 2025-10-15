3a6aedf6acfa89276f45dbc434c53c349229f27a
Scott Cardinali <scott.cardinali@gmail.com>
2025-10-15 16:26:16 -0400

    Checkers game is WIP but running

--- e2e/pages/CheckersPage.ts
+++ e2e/pages/CheckersPage.ts
@@ -1,21 +1,74 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class CheckersPage extends BasePage {
-    private readonly _title: Locator;
+  private readonly _title: Locator;
+  private readonly _message: Locator;
+  private readonly _restartLink: Locator;
+  private readonly _rulesLink: Locator;
+  private readonly _url = "https://www.gamesforthebrain.com/game/checkers/";
 
-    constructor(page: Page) {
-        super(page);
-        this._title = this.page.locator('h1');
-    }
+  constructor(page: Page) {
+    super(page);
+    this._title = page.locator('h1');
+    this._message = page.locator('#message');
+    this._restartLink = page.locator("text=Restart");
+    this._rulesLink = page.locator('a[href="https://en.wikipedia.org/wiki/English_draughts#Starting_position"]');
+  }
 
-    get title() {
-        return this._title.textContent();
-    }
+  get title() {
+    return this._title.textContent();
+  }
 
-    async goto() {
-        await this.navigateTo('https://www.gamesforthebrain.com/game/checkers/');
-        return this;
-    }
+  get message() {
+    return this._message.textContent();
+  }
 
+  getMessageLocator(msg: string) {
+    return this.page.locator(`#message:has-text("${msg}")`);
+  }
+
+  async goto() {
+    await this.navigateTo(this._url);
+    return this;
+  }
+
+  async clickRestartLink() {
+    await this._restartLink.click();
+
+  }
+
+  async clickRulesLink() {
+    await this._rulesLink.click();
+    await expect(this.page).toHaveURL('https://en.wikipedia.org/wiki/English_draughts#Starting_position');
+  }
+
+  async getChipsByOwner(owner: string) {
+    // Locate all the <img> elements within div.line inside #board
+    const elements = await this.page.locator('#board div.line img').elementHandles();
+
+    const chips = await Promise.all(
+      elements.map(async (el) => ({
+        src: (await el.getAttribute('src')) ?? '',
+        name: await el.getAttribute('name'),
+        onClick: await el.getAttribute('onclick'),
+      }))
+    );
+    // Filter chips by the owner
+    const chiparoos = chips.filter((c) => c.src.includes(`${owner}.gif`));
+    console.log(`Enemy pieces: ${JSON.stringify(chiparoos)}`);
+    return chiparoos;
+  }
+
+  async moveMyPieceAndWaitForMsg(start: number, end: number, msg: string, unselectedGif: string, selectedGif: string) {
+    // // Validate unselected orange piece before clicking
+    await expect(this.page.locator(`[name="space${start}"]`)).toHaveAttribute('src', unselectedGif);
+    await this.page.locator(`[name="space${start}"]`).click({ delay: 1000 });
+    // Validate orange piece is selected after click
+    await expect(this.page.locator(`[name="space${start}"]`)).toHaveAttribute('src', selectedGif);
+    await this.page.locator(`[name="space${end}"]`).click({ delay: 1000 });
+    // Validate unselected orange piece state after move
+    await expect(this.page.locator(`[name="space${end}"]`)).toHaveAttribute('src', unselectedGif);
+    await this.getMessageLocator(msg).isVisible();
+  }
 }
--- e2e/tests/checkers.spec.ts
+++ e2e/tests/checkers.spec.ts
@@ -2,13 +2,55 @@ import { expect, test } from '@playwright/test';
 import { CheckersPage } from '../pages/CheckersPage';
 
 test.describe('Checkers Page', () => {
+  const titleText = "Checkers";
+  const startGameMessage = "Select an orange piece to move.";
+  const moveMsg = "Make a move";
+  const selectedGif = "you1.gif";
+  const unSelectedGif = "you2.gif";
+
   let checkersPage: CheckersPage;
   test.beforeEach(async ({ page }) => {
     checkersPage = new CheckersPage(page);
     await checkersPage.goto();
   });
 
-  test('should have expected title', async () => {
-    await expect(await checkersPage.title).toBe("Checkers");
+  test('Validate the correct title', async () => {
+    await expect(checkersPage.title).resolves.toContain(titleText);
+  });
+
+  test('Validate the start game message', async () => {
+    await expect(checkersPage.message).resolves.toContain(startGameMessage);
+  });
+
+  test('Validate the Restart link', async () => {
+    await checkersPage.clickRestartLink();
+  });
+
+  test('Validate the Rules link', async () => {
+    await checkersPage.clickRulesLink();
+  });
+
+  const myMoves = [
+    { start: 62, end: 53 },
+    { start: 22, end: 33 },
+    { start: 71, end: 62 },
+    { start: 31, end: 13 },
+    { start: 62, end: 73 },
+    { start: 51, end: 62 },
+    { start: 11, end: 22 },
+    { start: 60, end: 71 },
+    { start: 40, end: 51 },
+    { start: 20, end: 31 },
+    { start: 53, end: 44 },
+    { start: 62, end: 44 },
+    { start: 13, end: 24 },
+  ];
+
+  test('Validates my moves can be played - Happy Path', async ({ page }) => {
+    for (const [index, move] of myMoves.entries()) {
+      await test.step(`Move ${index + 1}: ${move.start} → ${move.end}`, async () => {
+        await checkersPage.moveMyPieceAndWaitForMsg(move.start, move.end, moveMsg, unSelectedGif, selectedGif);
+      });
+    }
   });
 });
